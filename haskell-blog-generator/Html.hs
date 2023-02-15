5cb582c7c1f6b46a20bbfa69a6b3652445791440
LYK <dalinaum@gmail.com>
2023-02-16 00:41:24 +0900

    Init commit

---
+++ haskell-blog-generator/Html.hs
@@ -0,0 +1,16 @@
+module Html
+    (   Html
+    ,   Title
+    ,   Structure
+    ,   html_
+    ,   p_
+    ,   pre_
+    ,   h1_
+    ,   ul_
+    ,   ol_
+    ,   append_
+    ,   render
+    )
+    where
+
+import Html.Internal
---
+++ haskell-blog-generator/hello.hs
@@ -0,0 +1,16 @@
+import Html
+
+main :: IO ()
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml =
+    html_
+        "Hello title"
+        (   append_
+            (   h1_ "Hello, world!")
+            (   append_
+                (p_ "Paragraph #1")
+                (p_ "Paragraph #2")
+            )
+        )
