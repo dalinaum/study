1c7a1a872814b6d392496d771c4f2e121f0c1975
LYK <dalinaum@gmail.com>
2023-03-09 01:54:07 +0900

    FizzBuzz

--- elixir/ex5.exs
+++ elixir/ex5.exs
@@ -1,5 +1,16 @@
-list_concat = fn a , b -> a ++ b end
+list_concat = fn a, b -> a ++ b end
 
 sum = fn a, b, c -> a + b + c end
 
-pair_tuple_to_list = fn {a ,b} -> [a, b] end
+pair_tuple_to_list = fn {a, b} -> [a, b] end
+
+fizz_buzz_string = fn
+  0, 0, _ -> "FizzBuzz"
+  0, _, _ -> "Fizz"
+  _, 0, _ -> "Buzz"
+  _, _, rest -> rest
+end
+
+fizz_buzz = fn
+  n -> fizz_buzz_string.(rem(n, 3), rem(n, 5), n)
+end
