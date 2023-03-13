dd93cfc1106168a7800f19293f303503444a7759
LYK <dalinaum@gmail.com>
2023-03-14 01:13:25 +0900

    dynamic_nested 추가

---
+++ elixir/dynamic_nested.exs
@@ -0,0 +1,32 @@
+nested = %{
+  buttercup: %{
+    actor: %{
+      first: "Robin",
+      last: "Wright"
+    },
+    role: "princess"
+  },
+  westley: %{
+    actor: %{
+      first: "Cary",
+      # 오타!
+      last: "Elwis"
+    },
+    role: "farm boy"
+  }
+}
+
+IO.inspect get_in(nested, [:buttercup])
+# => %{actor: %{first: "Robin", last: "Wright"}, role: "princess"}
+
+IO.inspect get_in(nested, [:buttercup, :actor])
+# => %{first: "Robin", last: "Wright"}
+
+IO.inspect get_in(nested, [:buttercup, :actor, :first])
+# => "Robin"
+
+IO.inspect put_in(nested, [:westley, :actor, :last], "Elwes")
+# %{
+#   buttercup: %{actor: %{first: "Robin", last: "Wright"}, role: "princess"},
+#   westley: %{actor: %{first: "Cary", last: "Elwes"}, role: "farm boy"}
+# }
