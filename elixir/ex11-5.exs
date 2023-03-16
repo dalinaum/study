52d3eb887c20dd734588e053080203e51a591bf7
LYK <dalinaum@gmail.com>
2023-03-17 02:10:54 +0900

    capitalize_sentences  추가

--- elixir/ex11-5.exs
+++ elixir/ex11-5.exs
@@ -19,4 +19,35 @@ defmodule MyCharList do
     IO.puts(String.pad_leading(head, new_length))
     _center(tail, max_length)
   end
+
+  def capitalize_sentences(str), do: _capitalize_sentences(str, true, true, << >>)
+
+  def _capitalize_sentences(<<>>, _after_dot, _after_space, str), do: str
+
+  def _capitalize_sentences(<<head::utf8, tail::binary>>, after_dot, after_space, str) do
+    if head == ?. do
+      str = <<str::binary, head::utf8>>
+      _capitalize_sentences(tail, true, false, str)
+    else
+      if after_dot and head == 32 do
+        str = <<str::binary, head::utf8>>
+        _capitalize_sentences(tail, true, true, str)
+      else
+        if after_dot and after_space do
+          head = head - ?a + ?A
+          str = <<str::binary, head::utf8>>
+          _capitalize_sentences(tail, false, false, str)
+        else
+          if head >= ?A and head <= ?Z do
+            head = head - ?A + ?a
+            str = <<str::binary, head::utf8>>
+            _capitalize_sentences(tail, false, false, str)
+          else
+            str = <<str::binary, head::utf8>>
+            _capitalize_sentences(tail, false, false, str)
+          end
+        end
+      end
+    end
+  end
 end
