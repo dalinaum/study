2d11ee3cce959471d7f02fb8dfa1bb64dd0b95b0
LYK <dalinaum@gmail.com>
2023-03-21 19:46:00 +0900

    mix docs 추가

--- elixir/issues/mix.exs
+++ elixir/issues/mix.exs
@@ -8,6 +8,8 @@ defmodule Issues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      name: "Issues",
+      source_url: "https://github.com/pragdave/issues",
       deps: deps()
     ]
   end
@@ -23,7 +25,9 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.0"},
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.25"},
+      {:earmark, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
