cd660c0399950ed69206f90df253b7d68ac457b6
LYK <dalinaum@gmail.com>
2023-03-22 03:17:47 +0900

    속성 테스트 추가

--- elixir/issues/mix.exs
+++ elixir/issues/mix.exs
@@ -27,7 +27,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.0"},
       {:poison, "~> 5.0"},
       {:ex_doc, "~> 0.25"},
-      {:earmark, "~> 1.4"}
+      {:earmark, "~> 1.4"},
+      {:stream_data, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
