aaaab2f15b67ca2666720890824175b6245e5e14
LYK <dalinaum@gmail.com>
2023-04-07 23:55:16 +0900

    chains.exe 파일도 추가

---
+++ elixir/spawn/chain.exs
@@ -0,0 +1,17 @@
+defmodule Chain do
+  def counter(next_pid) do
+    receive do
+      n ->
+        send next_pid, n + 1
+    end
+  end
+
+  def create_processes(n) do
+    code_to_run = fn (_, send_to) ->
+      spawn(Chain, :counter, [send_to])
+    end
+
+    last = Enum.reduce(1..n, self(), code_to_run)
+    send(last, 0) # 마지막 프로세스에 0을 보내 카운트를 시작한다.
+  end
+end
