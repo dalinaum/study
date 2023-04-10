290af5c3f7e54555acfdbf671600ded7bd749a07
LYK <dalinaum@gmail.com>
2023-04-10 14:52:06 +0900

    가장 짧은 구현까지

--- algo/34.py
+++ algo/34.py
@@ -1,25 +1,26 @@
 from typing import List
+import itertools
 
 # 45ms. 처음만든 방식. 책은 반대로 빼어가면서 만드는 듯?
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(permutaion: List[int] = []):
-            if len(permutaion) == len(nums):
-                result.append(permutaion)
-                return
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def dfs(permutaion: List[int] = []):
+#             if len(permutaion) == len(nums):
+#                 result.append(permutaion)
+#                 return
 
-            for i in nums:
-                if i not in permutaion:
-                    new_permutation = permutaion.copy()
-                    new_permutation.append(i)
-                    dfs(new_permutation)
+#             for i in nums:
+#                 if i not in permutaion:
+#                     new_permutation = permutaion.copy()
+#                     new_permutation.append(i)
+#                     dfs(new_permutation)
       
-        if not nums:
-            return []
+#         if not nums:
+#             return []
         
-        result = []
-        dfs()
-        return result
+#         result = []
+#         dfs()
+#         return result
 
 # 34ms 이게 훨씬 빠르네?!
 # class Solution:
@@ -43,26 +44,30 @@ class Solution:
 #         return result
 
 # 40ms 책의 풀이. 완전히 생각도 못한 스타일. 내 풀이가 나은 듯.
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         prev_elements: List[int] = []
+#         result: List[List[int]] = []
+
+#         def dfs(elements: List[int] = []):
+#             if len(elements) == 0:
+#                 result.append(prev_elements[:])
+#                 return
+            
+#             for i in elements:
+#                 next_elements = elements[:]
+#                 next_elements.remove(i)
+#                 prev_elements.append(i)
+#                 dfs(next_elements)
+#                 prev_elements.pop()
+        
+#         dfs(nums)
+#         return result
+
+# 43ms ~ 26ms 짧은 구현.
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        prev_elements: List[int] = []
-        result: List[List[int]] = []
-
-        def dfs(elements: List[int] = []):
-            if len(elements) == 0:
-                result.append(prev_elements[:])
-                return
-            
-            for i in elements:
-                next_elements = elements[:]
-                next_elements.remove(i)
-                prev_elements.append(i)
-                dfs(next_elements)
-                prev_elements.pop()
-        
-        dfs(nums)
-        return result
-
+        return list(itertools.permutations(nums))
 
 nums = [1,2,3]
 
