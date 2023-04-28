370217969b8856a0b0af4b1dd2c8e8a300c692e2
LYK <dalinaum@gmail.com>
2023-04-29 01:00:11 +0900

    message_length를 Admin으로 옮김.

--- askcompany/askcompany/instagram/admin.py
+++ askcompany/askcompany/instagram/admin.py
@@ -7,3 +7,6 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'message', 'message_length', 'created_at', 'updated_at']
     list_display_links = ['id', 'message']
+
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
