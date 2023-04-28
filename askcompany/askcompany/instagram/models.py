a8096127ea8e4ebb77c152dca73a8a10f71766c3
LYK <dalinaum@gmail.com>
2023-04-29 00:23:28 +0900

    __str__을 구현해서 어드민에서 디폴트로 보이는 모양을 변경.

--- askcompany/askcompany/instagram/models.py
+++ askcompany/askcompany/instagram/models.py
@@ -3,5 +3,11 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # return f"Custom Post object ({self.id})"
+        # return "Custom Post object (%s)" % self.id
+        # return "Custom Post object ({})".format(self.id)
+        return self.message
