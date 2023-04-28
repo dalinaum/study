9defbbd06fd681e7a89f9bcf33582f33b49cb9a4
LYK <dalinaum@gmail.com>
2023-04-29 01:00:34 +0900

    스태틱 메서드로 변경.

--- askcompany/askcompany/instagram/admin.py
+++ askcompany/askcompany/instagram/admin.py
@@ -8,5 +8,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'message', 'message_length', 'created_at', 'updated_at']
     list_display_links = ['id', 'message']
 
-    def message_length(self, post):
+    @staticmethod
+    def message_length(post):
         return f"{len(post.message)} 글자"
