cf2956831c89be0a10f96269831c932d07dc3858
LYK <dalinaum@gmail.com>
2023-04-29 00:23:53 +0900

    인스타의 Post를 Admin에 등록

--- askcompany/askcompany/instagram/admin.py
+++ askcompany/askcompany/instagram/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+# admin.site.register(Post)
+# admin.site.register(Post, PostAdmin)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'created_at', 'updated_at']
+    list_display_links = ['id', 'message']
