3b74f75d7c3c84240d6b5337871e9296881744dc
LYK <dalinaum@gmail.com>
2023-04-29 01:10:35 +0900

    Search filed에 대한 설명 추가

--- askcompany/askcompany/instagram/admin.py
+++ askcompany/askcompany/instagram/admin.py
@@ -7,7 +7,17 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'message', 'message_length', 'created_at', 'updated_at']
     list_display_links = ['id', 'message']
+    search_fields = ['message']
 
     @staticmethod
     def message_length(post):
         return f"{len(post.message)} 글자"
+
+# >>> from instagram.models import Post
+# >>> Post.objects.all()
+# <QuerySet [<Post: 첫번째 메시지>, <Post: 두번째 메시지>, <Post: 세번째 메시지>]>
+# >>> Post.objects.all().filter(message__icontains='첫번째')
+# <QuerySet [<Post: 첫번째 메시지>]>
+# >>> qs =Post.objects.all().filter(message__icontains='첫번째')
+# >>> print(qs.query)
+# SELECT "instagram_post"."id", "instagram_post"."message", "instagram_post"."created_at", "instagram_post"."updated_at" FROM "instagram_post" WHERE "instagram_post"."message" LIKE %첫번째% ESCAPE '\'
