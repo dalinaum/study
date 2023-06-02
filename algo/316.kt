61914264c990229dc105c4be8788fa709f613d24
LYK <dalinaum@gmail.com>
2023-06-03 01:41:32 +0900

    Update 316.kt

--- algo/316.kt
+++ algo/316.kt
@@ -1,13 +1,44 @@
+// class Solution {
+//     fun removeDuplicateLetters(s: String): String {
+//         val sSet = s.toSet()
+//         for (ch in sSet.sorted()) {
+//             val suffix = s.substring(s.indexOf(ch))
+//             if (sSet == suffix.toSet()) {
+//                 return ch + removeDuplicateLetters(suffix.replace(ch.toString(), ""))
+//             }
+//         }
+//         return ""
+//     }
+// }
+
+// //val s = "bcabc"
+// //val s = "bcdabc"
+// val s = "cbacdcbc"
+
+println(Solution().removeDuplicateLetters(s))
+
 class Solution {
     fun removeDuplicateLetters(s: String): String {
-        val sSet = s.toSet()
-        for (ch in sSet.sorted()) {
-            val suffix = s.substring(s.indexOf(ch))
-            if (sSet == suffix.toSet()) {
-                return ch + removeDuplicateLetters(suffix.replace(ch.toString(), ""))
-            }
+        val seen = mutableSetOf<Char>()
+        val counts = HashMap<Char, Int>()
+        for (ch in s) {
+            counts[ch] = counts.getOrDefault(ch, 0) + 1
         }
-        return ""
+
+        val stack = mutableListOf<Char>()
+        for (ch in s) {
+            counts[ch] = counts.getOrDefault(ch, 0) - 1 // 디폴트 값이 나올 수 없음.
+            if (seen.contains(ch)) continue
+
+            while (stack.isNotEmpty() && stack[stack.lastIndex] > ch && counts.getOrDefault(stack[stack.lastIndex], 0) > 0) {
+                val lastCh = stack.removeAt(stack.lastIndex)
+                seen.remove(lastCh)
+            }
+
+            stack.add(ch)
+            seen.add(ch)
+        }
+        return stack.joinToString("")
     }
 }
 
