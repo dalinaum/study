2c680d2068b4f40a2e0ca6479041497db07d1001
LYK <dalinaum@gmail.com>
2023-06-06 02:00:28 +0900

    Update 787.kt

--- algo/787.kt
+++ algo/787.kt
@@ -13,6 +13,11 @@ data class QueueItem(
     }
 }
 
+data class PriceAndStop(
+    val price: Int,
+    val stop: Int
+)
+
 class Solution {
     fun findCheapestPrice(n: Int, flights: Array<IntArray>, src: Int, dst: Int, k: Int): Int {
         val graph = mutableMapOf<Int, MutableList<DestinationAndPrice>>()
@@ -20,8 +25,12 @@ class Solution {
             val value = graph.getOrDefault(from, mutableListOf())
             value.add(DestinationAndPrice(to, price))
             graph[from] = value
-            graph.putIfAbsent(to, mutableListOf())
         }
+
+        val prices = Array<PriceAndStop>(n) {
+            PriceAndStop(Int.MAX_VALUE, 0)
+        }
+
         val queue = PriorityQueue<QueueItem>()
         queue.add(QueueItem(0, src, 0))
         while (queue.isNotEmpty()) {
@@ -30,8 +39,14 @@ class Solution {
                 return queueItem.price
             }
             if (queueItem.stop <= k) {
-                for ((dest, price) in graph.getValue(queueItem.dest)) {
-                    queue.add(QueueItem(price + queueItem.price, dest, queueItem.stop + 1))
+                val stop = queueItem.stop + 1
+                for ((dest, price) in graph.getOrDefault(queueItem.dest, mutableListOf())) {
+                    val newPrice = price + queueItem.price
+                    val oldPrice = prices[dest]
+                    if (oldPrice.price > newPrice || oldPrice.stop >= stop) {
+                        prices[dest] = PriceAndStop(newPrice, stop)
+                        queue.add(QueueItem(newPrice, dest, stop))
+                    }
                 }
             }
         }
@@ -57,10 +72,16 @@ class Solution {
 //val dst = 2
 //val k = 1
 
-val n = 3
-val flights = arrayOf(intArrayOf(0,1,100),intArrayOf(1,2,100),intArrayOf(0,2,500))
+//val n = 3
+//val flights = arrayOf(intArrayOf(0, 1, 100), intArrayOf(1, 2, 100), intArrayOf(0, 2, 500))
+//val src = 0
+//val dst = 2
+//val k = 0
+
+val n = 5
+val flights = arrayOf(intArrayOf(1, 2, 4))
 val src = 0
-val dst = 2
-val k = 0
+val dst = 4
+val k = 2
 
 println(Solution().findCheapestPrice(4, flights, src, dst, k))
