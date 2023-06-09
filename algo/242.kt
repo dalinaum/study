d851cd6267503e8b4f40f709bba0098371c8dd37
LYK <dalinaum@gmail.com>
2023-06-09 23:14:22 +0900

    Update 242.kt

--- algo/242.kt
+++ algo/242.kt
@@ -1,6 +1,13 @@
+// 리스트나 어레이로 바꾸고 소트하면 성능이 파이썬보다 떨어진다.
 // class Solution {
 //     fun isAnagram(s: String, t: String): Boolean {
 //         return s.toMutableList().sorted() == t.toMutableList().sorted()
+//     }
+// }
+
+// class Solution {
+//     fun isAnagram(s: String, t: String): Boolean {
+//         return s.toCharArray().sorted() == t.toCharArray().sorted()
 //     }
 // }
 
