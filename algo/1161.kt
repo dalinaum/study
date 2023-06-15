209411df95eea2ff787cb5db456037f42719274a
LYK <dalinaum@gmail.com>
2023-06-16 01:38:05 +0900

    Create 1161.kt

---
+++ algo/1161.kt
@@ -0,0 +1,55 @@
+import kotlin.math.max
+
+data class TreeNode(
+    var `val`: Int,
+    var left: TreeNode? = null,
+    var right: TreeNode? = null
+)
+
+class Solution {
+    fun maxLevelSum(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+        val sizes = mutableMapOf<Int, Int>()
+        val queue = mutableListOf<Pair<Int, TreeNode>>()
+        queue.add(1 to root)
+
+        while (queue.isNotEmpty()) {
+            val (level, node) = queue.removeAt(0)
+            val size = sizes.getOrDefault(level, 0) + node.`val`
+            sizes[level] = size
+            if (node.left != null) {
+                queue.add(level + 1 to node.left!!)
+            }
+            if (node.right != null) {
+                queue.add(level + 1 to node.right!!)
+            }
+        }
+
+        return sizes.maxBy { it.value }.key
+    }
+}
+
+val head = TreeNode(1)
+head.left = TreeNode(7)
+head.right = TreeNode(0)
+head.left?.left = TreeNode(7)
+head.left?.right = TreeNode(-8)
+
+//[1,0,0,7,8,null,null]
+//val head = TreeNode(1)
+//head.left = TreeNode(0)
+//head.right = TreeNode(0)
+//head.left?.left = TreeNode(7)
+//head.left?.right = TreeNode(8)
+
+//[989,null,10250,98693,-89388,null,null,null,-32127]
+//val head = TreeNode(989)
+//head.right = TreeNode(10250)
+//head.right?.left = TreeNode(98693)
+//head.right?.right = TreeNode(-89388)
+//head.right?.right?.right = TreeNode(-32127)
+
+
+println(Solution().maxLevelSum(head))
