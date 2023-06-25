68ed6e5fa3812d5d1843eca2bd88cd4f0bccf667
LYK <dalinaum@gmail.com>
2023-06-26 00:30:05 +0900

    RoomWordSample 추가

---
+++ RoomWordSample/app/src/main/java/io/github/dalinuam/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,36 @@
+package io.github.dalinuam.roomwordsample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+
+class NewWordActivity : AppCompatActivity() {
+    lateinit var editWordView: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+        editWordView = findViewById(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (editWordView.text.isEmpty()) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "io.github.dalinuam.roomwordsample.REPLY"
+    }
+}
