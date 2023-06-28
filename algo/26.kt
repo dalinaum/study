07abf14356de8f3406798aadede71509790620ba
LYK <dalinaum@gmail.com>
2023-06-29 01:00:20 +0900

    Update 26.kt

--- algo/26.kt
+++ algo/26.kt
@@ -1,13 +1,31 @@
+// Set을 쓴 방법
+
+// class Solution {
+//     fun removeDuplicates(nums: IntArray): Int {
+//         val seen = mutableSetOf<Int>()
+//         var skipped = 0
+//         for ((i, num) in nums.withIndex()) {
+//             if (num in seen) {
+//                 skipped++
+//             } else {
+//                 nums[i - skipped] = num
+//                 seen.add(num)
+//             }
+//         }
+//         return nums.size - skipped
+//     }
+// }
+
+// 이전 값과 비교
+
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        val seen = mutableSetOf<Int>()
         var skipped = 0
         for ((i, num) in nums.withIndex()) {
-            if (num in seen) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 skipped++
             } else {
                 nums[i - skipped] = num
-                seen.add(num)
             }
         }
         return nums.size - skipped
