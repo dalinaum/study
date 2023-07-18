e4f5da93f42626f5c56b1b40a7757da61902f9d6
LYK <dalinaum@gmail.com>
2023-07-19 00:51:15 +0900

    메모이제이션

--- react-project2/src/component/TodoList.js
+++ react-project2/src/component/TodoList.js
@@ -1,21 +1,43 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo, onUpdate, onDelete }) => {
     const [search, setSearch] = useState("");
+
     const onChangeSearch = (e) => {
         setSearch(e.target.value);
     };
+
     const getSearchResult = () => {
         return search === ""
             ? todo
             : todo.filter((it) =>
                 it.content.toLowerCase().includes(search.toLowerCase()));
     };
+
+    const analyzeTodo = useMemo(() => {
+        console.log("analyzeTodo 함수 호출");
+        const totalCount = todo.length;
+        const doneCount = todo.filter((it) => it.isDone).length;
+        const notDoneCount = totalCount - doneCount;
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount,
+        };
+    }, [todo]);
+
+    const { totalCount, doneCount, notDoneCount } = analyzeTodo;
+
     return (
         <div className="TodoList">
-            <h4>Todo List</h4>
+            <h4>Todo List 🍒</h4>
+            <div>
+                <div>총개수: {totalCount}</div>
+                <div>완료된 할 일: {doneCount}</div>
+                <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+            </div>
             <input
                 value={search}
                 onChange={onChangeSearch}
