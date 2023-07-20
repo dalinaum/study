4bfa990d36afa5f1c0f543bbea9cd9ed79efcda3
LYK <dalinaum@gmail.com>
2023-07-21 01:27:01 +0900

    react-project3에서 라우트 기본 세팅까지

--- react-project3/src/App.js
+++ react-project3/src/App.js
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="/edit" element={<Edit />} />
+    </Routes>
+  </div>
 }
 
 export default App;
