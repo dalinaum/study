d5c76b9e4e62d56562e578bc77de48db6b2d7bf8
LYK <dalinaum@gmail.com>
2023-07-25 02:42:30 +0900

    일기 입력 구현

--- react-project3/src/pages/Home.js
+++ react-project3/src/pages/Home.js
@@ -1,29 +1,12 @@
-import Button from "../components/Button";
-import Header from "../components/Header";
+import Editor from "../components/Editor"
 
 const Home = () => {
     return (
         <div>
-            <Header
-                title={"Home"}
-                leftChild={
-                    <Button
-                        type="positive"
-                        text={"긍정 버튼"}
-                        onClick={() => {
-                            alert("positive button");
-                        }}
-                    />
-                }
-                rightChild={
-                    <Button
-                        type="negative"
-                        text={"부정s 버튼"}
-                        onClick={() => {
-                            alert("negative button");
-                        }}
-                    />
-                }
+            <Editor
+                onSubmit={() => {
+                    alert("작성 완료 버튼을 클릭했음");
+                }}
             />
         </div>
     );
