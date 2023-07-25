21cec930bd0c35b9a325576a4d555ce671784e14
LYK <dalinaum@gmail.com>
2023-07-26 00:27:50 +0900

    테스트용 코드 제거

--- react-project3/src/pages/Home.js
+++ react-project3/src/pages/Home.js
@@ -1,19 +1,6 @@
-import Editor from "../component/Editor"
-
 const Home = () => {
     return (
-        <div>
-            <Editor
-                initData={{
-                    data: new Date().getTime(),
-                    emotionId: 1,
-                    content: "이전에 작성했던 일기",
-                }}
-                onSubmit={() => {
-                    alert("작성 완료 버튼을 클릭했음");
-                }}
-            />
-        </div>
+        <div>Home 페이지입니다</div>
     );
 };
 
