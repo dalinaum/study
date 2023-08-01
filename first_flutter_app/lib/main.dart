92af990eadbe363fa6729100383187b05074b926
LYK <dalinaum@gmail.com>
2023-08-01 23:31:50 +0900

    불필요한 로그를 삽입.

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -8,7 +8,10 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<StatefulWidget> createState() => _MyApp();
+  State<StatefulWidget> createState() {
+    // print('createState');
+    return _MyApp();
+  }
 }
 
 class _MyApp extends State<MyApp> {
@@ -17,7 +20,9 @@ class _MyApp extends State<MyApp> {
   Color _color = Colors.blue;
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) {
+    // print('build');
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -46,4 +51,17 @@ class _MyApp extends State<MyApp> {
           ),
         ),
       ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // print('initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // print('didChangeDependencies');
+  }
 }
