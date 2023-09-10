f04fb0a96ca98e6f3b532a4b75b1032ae8d3720d
LYK <dalinaum@gmail.com>
2023-09-11 03:22:54 +0900

    Update theme.

--- bird-app/shared/src/commonMain/kotlin/App.kt
+++ bird-app/shared/src/commonMain/kotlin/App.kt
@@ -9,36 +9,46 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.serialization.kotlinx.json.json
 import model.BirdImage
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+
+@Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdsViewModel() })
         BirdsPage(birdsViewModel)
     }
@@ -57,9 +67,16 @@ fun BirdsPage(viewModel: BirdsViewModel) {
             horizontalArrangement = Arrangement.spacedBy(5.dp)
         ) {
             for (category in uiState.categories) {
-                Button(onClick = {
-                    viewModel.selectCategory(category)
-                }) {
+                Button(
+                    onClick = {
+                        viewModel.selectCategory(category)
+                    },
+                    modifier = Modifier.aspectRatio(1.0f).fillMaxSize().weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        focusedElevation = 0.dp
+                    )
+                ) {
                     Text(category)
                 }
             }
