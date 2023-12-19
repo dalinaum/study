0a5fe3a761079df76745b2e286d547c8538a685a
LYK <dalinaum@gmail.com>
2023-12-20 02:14:08 +0900

    opacity 에니메이션 추가

--- flutter/animation_example/lib/main.dart
+++ flutter/animation_example/lib/main.dart
@@ -34,6 +34,7 @@ class _AnimationApp extends State<AnimationApp> {
   final List<Person> _people = List.empty(growable: true);
   Color _weightColor = Colors.blue;
   var _current = 0;
+  var _opacity = 1.0;
 
   @override
   void initState() {
@@ -57,49 +58,53 @@ class _AnimationApp extends State<AnimationApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    width: 100,
-                    child: Text('이름: ${_people[_current].name}'),
-                  ),
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 2),
-                    curve: Curves.bounceIn,
-                    color: Colors.amber,
-                    width: 50,
-                    height: _people[_current].height,
-                    child: Text(
-                      '키 ${_people[_current].height}',
-                      textAlign: TextAlign.center,
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: const Duration(seconds: 1),
+              child: SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      child: Text('이름: ${_people[_current].name}'),
                     ),
-                  ),
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 2),
-                    curve: Curves.easeInCubic,
-                    color: _weightColor,
-                    width: 50,
-                    height: _people[_current].weight,
-                    child: Text(
-                      '몸무게 ${_people[_current].weight}',
-                      textAlign: TextAlign.center,
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.bounceIn,
+                      color: Colors.amber,
+                      width: 50,
+                      height: _people[_current].height,
+                      child: Text(
+                        '키 ${_people[_current].height}',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ),
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 2),
-                    curve: Curves.linear,
-                    color: Colors.pinkAccent,
-                    width: 50,
-                    height: _people[_current].bmi,
-                    child: Text(
-                      'bmi ${_people[_current].bmi.toString().substring(0, 2)}',
-                      textAlign: TextAlign.center,
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.easeInCubic,
+                      color: _weightColor,
+                      width: 50,
+                      height: _people[_current].weight,
+                      child: Text(
+                        '몸무게 ${_people[_current].weight}',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ),
-                ],
+                    AnimatedContainer(
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.linear,
+                      color: Colors.pinkAccent,
+                      width: 50,
+                      height: _people[_current].bmi,
+                      child: Text(
+                        'bmi ${_people[_current].bmi.toString().substring(0, 2)}',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             ElevatedButton(
@@ -123,7 +128,15 @@ class _AnimationApp extends State<AnimationApp> {
                 });
               },
               child: const Text('이전'),
-            )
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _opacity == 1 ? _opacity = 0 : _opacity = 1;
+                });
+              },
+              child: const Text('사라지기')
+            ),
           ],
         ),
       ),
