563c07ee4ffd0d5cc98b32a65bc612fc68163ba2
LYK <dalinaum@gmail.com>
2024-02-24 03:20:31 +0900

    서버를 파일 DB에 맞춰 변경

--- shopping-mall/server/src/resolvers/cart.ts
+++ shopping-mall/server/src/resolvers/cart.ts
@@ -6,18 +6,16 @@ const setJSON = (data: Cart) => writeDB(DBField.CART, data)
 const cartResolver: Resolver = {
     Query: {
         cart: (parent, args, { db }) => {
-            return db.cart 
+            return db.cart
         }
     },
     Mutation: {
-        addCart: (parent, { id }, { db }, info) => {
+        addCart: (parent, { id }, { db }) => {
             if (!id) throw Error('상품id가 없다')
             const targetProduct = db.products.find(item => item.id === id)
-
             if (!targetProduct) {
                 throw new Error('상품이 없습니다')
             }
-
             const existCartItemIndex = db.cart.findIndex(item => item.id === id)
             if (existCartItemIndex > -1) {
                 const newCartItem = {
@@ -28,7 +26,6 @@ const cartResolver: Resolver = {
                 setJSON(db.cart)
                 return newCartItem
             }
-
             const newItem = {
                 id,
                 amount: 1,
@@ -38,35 +35,37 @@ const cartResolver: Resolver = {
             setJSON(db.cart)
             return newItem
         },
-        // updateCart: (parent, { id, amount }, context, info) => {
-        //     const newData = { ...cartData }
-        //     if (!newData[id]) {
-        //         throw new Error('없는 데이터입니다')
-        //     }
-        //     const newItem = {
-        //         ...newData[id],
-        //         amount,
-        //     }
-        //     newData[id] = newItem
-        //     cartData = newData
-        //     return newItem
-        // },
-        // deleteCart: (parent, { id }, context, info) => {
-        //     const newData = { ...cartData }
-        //     delete newData[id]
-        //     cartData = newData
-        //     return id
-        // },
-        // executePay: (parent, { ids }, context, info) => {
-        //     const newCartData = cartData.filter(cartItem =>
-        //         !ids.include(cartItem.id)
-        //     )
-        //     cartData = newCartData
-        //     return ids
-        // }
+
+        updateCart: (parent, { id, amount }, { db }) => {
+            const existCartItemIndex = db.cart.findIndex(item => item.id === id)
+            if (existCartItemIndex < 0) {
+                throw new Error('없는 데이터입니다')
+            }
+            const newCartItem = { id, amount }
+            db.cart.splice(existCartItemIndex, 1, newCartItem)
+            setJSON(db.cart)
+            return newCartItem
+        },
+        deleteCart: (parent, { id }, { db }) => {
+            const existCartItemIndex = db.cart.findIndex(item => item.id === id)
+            if (existCartItemIndex < 0) {
+                throw new Error('없는 데이터입니다')
+            }
+            db.cart.splice(existCartItemIndex, 1)
+            setJSON(db.cart)
+            return id
+        },
+        executePay: (parent, { ids }, { db }) => {
+            const newCartData = db.cart.filter(cartItem =>
+                !ids.includes(cartItem.id))
+            db.cart = newCartData
+            setJSON(db.cart)
+            return ids
+        }
     },
     CartItem: {
-        product: (cartItem, args, { db }) => db.products.find((product: any) => product.id === cartItem.id)
+        product: (cartItem, args, { db }) =>
+            db.products.find((product: any) => product.id === cartItem.id)
     }
 }
 
