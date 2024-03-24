54a1e1d87881ebff73e4e838e812fc5156cddc7b
LYK <dalinaum@gmail.com>
2024-03-25 00:47:50 +0900

    useIntersection 훅으로 분리.

--- shopping-mall/client/src/pages/products/index.tsx
+++ shopping-mall/client/src/pages/products/index.tsx
@@ -1,26 +1,13 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 import { useInfiniteQuery } from "react-query"
 import { graphqlFetcher, QueryKeys } from "../../queryClient"
 import GET_PRODUCTS, { Products } from "../../graphql/products"
 import ProductList from "../../components/product/list"
+import useIntersection from "../../hooks/useIntersection"
 
 const ProductListPage = () => {
-    const observerRef = useRef<IntersectionObserver>()
     const fetchMoreRef = useRef<HTMLDivElement>(null)
-    const [intersecting, setIntersecting] = useState(false)
-
-    const getObserver = useCallback(() => {
-        if (!observerRef.current) {
-            observerRef.current = new IntersectionObserver(entries => {
-                setIntersecting(entries[0]?.isIntersecting)
-            })
-        }
-        return observerRef.current
-    }, [observerRef.current])
-
-    useEffect(() => {
-        if (fetchMoreRef.current) getObserver().observe(fetchMoreRef.current)
-    }, [fetchMoreRef.current])
+    const intersecting = useIntersection(fetchMoreRef)
 
     const { data, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
         QueryKeys.PRODUCTS,
