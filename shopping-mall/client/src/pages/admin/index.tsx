4cb6ff661d1da54fb5d7e50ca63c7ee35e72de72
LYK <dalinaum@gmail.com>
2024-04-11 02:32:12 +0900

    관리자 페이지 추가, 삭제된 건 관리자만
    관리자 페이지 추가, 삭제된 내용이 관리자에서만 보이도록 변경, 관리자와 프로덕츠의 질의 키를 다르게, GQL에서도 products의 파라미터에 showDeleted 추가,

--- shopping-mall/client/src/pages/admin/index.tsx
+++ shopping-mall/client/src/pages/admin/index.tsx
@@ -1,3 +1,39 @@
-const AdminPage = () => <div>어드민</div>
+import { useEffect, useRef } from "react"
+import { useInfiniteQuery } from "react-query"
+import { graphqlFetcher, QueryKeys } from "../../queryClient"
+import GET_PRODUCTS, { Products } from "../../graphql/products"
+import ProductList from "../../components/product/list"
+import useIntersection from "../../hooks/useIntersection"
+
+const AdminPage = () => {
+    const fetchMoreRef = useRef<HTMLDivElement>(null)
+    const intersecting = useIntersection(fetchMoreRef)
+
+    const { data, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
+        [QueryKeys.PRODUCTS, true],
+        ({ pageParam = '' }) => graphqlFetcher<Products>(GET_PRODUCTS, {
+            cursor: pageParam,
+            showDeleted: true
+        }),
+        {
+            getNextPageParam: lastPage => {
+                return lastPage.products.at(-1)?.id
+            }
+        }
+    )
+
+    useEffect(() => {
+        if (!intersecting || !isSuccess || !hasNextPage || isFetchingNextPage) return
+        fetchNextPage()
+    }, [intersecting])
+
+    return (
+        <div>
+            <h2>상품목록</h2>
+            <ProductList list={data?.pages || []} />
+            <div ref={fetchMoreRef}></div>
+        </div>
+    )
+}
 
 export default AdminPage
