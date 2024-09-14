d37e32ea3863fe81deb85f07bc02922b962d3268
LYK <dalinaum@gmail.com>
2024-09-15 04:37:30 +0900

    별도의 리포지토리로 분리

--- shopping-mall/client/src/util/arrToObj.ts
+++
@@ -1,8 +0,0 @@
-const arrToObj = (arr: [string, any][]) => {
-    return arr.reduce<{ [key: string]: any }>((res, [key, val]) => {
-        res[key] = val
-        return res
-    }, [])
-}
-
-export default arrToObj
--- shopping-mall/server/src/resolvers/index.ts
+++
@@ -1,4 +0,0 @@
-import productResolver from "./product"
-import cartResolver from "./cart"
-
-export default [productResolver, cartResolver]
